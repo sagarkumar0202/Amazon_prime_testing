2f6ac28655f3f30601a8f180721789a01630b471
Sagar Jha <96566831+sagarkumar0202@users.noreply.github.com>
2025-07-13 10:27:20 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    movies = ['Kalki', 'Asur', 'Panchayat(S4)', 'Ace', 'Stree 2']
+    return render_template('home.html', movies=movies)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
